326547a65cc0ca4525640a7b9d477120e629873a
Robbe Van der Gucht <robbe.vandergucht@gmail.com>
2015-02-02 21:33:21 +0100

    IPv4 datagram header

--- ethersniffer.py
+++ ethersniffer.py
@@ -5,6 +5,10 @@ from struct import *
 # struct doc: http://docs.activestate.com/activepython/3.0/python/library/struct.html
 # ethernet:  http://networksorcery.com/enp/protocol/ethernet.htm
 
+class Invalid_datagram_exception(Exception):
+  def __init__(self, msg):
+    self.message = "Invalid datagram %s" % msg
+
 class Ethernet_frame:
   """Ethernet 802.3 Packet format"""
   from socket import ntohs
@@ -56,39 +60,9 @@ class IPv4:
    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
                     Example Internet Datagram Header
-  version:  4 bits
-  IHL:  4 bits
-
-    Internet Header Length is the length of the internet header in 32
-    bit words, and thus points to the beginning of the data.  Note that
-    the minimum value for a correct header is 5.
-
-  Type of Service:  8 bits
-
-    The Type of Service provides an indication of the abstract
-    parameters of the quality of service desired.  These parameters are
-    to be used to guide the selection of the actual service parameters
-    when transmitting a datagram through a particular network.  Several
-    networks offer service precedence, which somehow treats high
-    precedence traffic as more important than other traffic (generally
-    by accepting only traffic above a certain precedence at time of high
-    load).  The major choice is a three way tradeoff between low-delay,
-    high-reliability, and high-throughput.
-
-      Bits 0-2:  Precedence.
-      Bit    3:  0 = Normal Delay,      1 = Low Delay.
-      Bits   4:  0 = Normal Throughput, 1 = High Throughput.
-      Bits   5:  0 = Normal Relibility, 1 = High Relibility.
-      Bit  6-7:  Reserved for Future Use.
-
-         0     1     2     3     4     5     6     7
-      +-----+-----+-----+-----+-----+-----+-----+-----+
-      |                 |     |     |     |     |     |
-      |   PRECEDENCE    |  D  |  T  |  R  |  0  |  0  |
-      |                 |     |     |     |     |     |
-      +-----+-----+-----+-----+-----+-----+-----+-----+
-
   """
+  from socket import ntohs, inet_ntoa
+  from struct import unpack
   TypeOfServicePrecedence = { 0b111: "Network Control",
                               0b110: "Internetwork Control",
                               0b101: "CRITIC/ECP",
@@ -97,6 +71,51 @@ class IPv4:
                               0b010: "Immediate",
                               0b001: "Priority",
                               0b000: "Routine" }
+  def __init__(self, raw_datagram):
+    vihl = unpack("!B", raw_datagram[:1]) # version + ip header length
+    self.version = vihl[0] >> 4
+    self.ihl     = vihl[0] & 0x0F # ihl => number of 32 bit words in header (max: 15)
+    print("IHL: %d" % self.ihl)
+
+    if self.ihl < 5: # less then minimum length
+      raise Invalid_datagram_exception("IHL is less than minimum(5). IHL :: " + str(self.ihl))
+
+    unpackstr = '!BBHHHBBH4s4s' # for ihl == 5
+    # ! - big-endian (network std)
+    # B - unsigned char   (int)    8 bits
+    # H - unsigned short  (int)    16 bits
+    # s - char[]          (bytes)  8[] bits
+
+    if self.ihl == 5: # minimum
+      ipheader = unpack(unpackstr, raw_datagram[:5 * 4]) # 5 * 4 bytes | 5 * 32 bits
+      self.type_of_service = ipheader[1] # TODO: details page 12 RFC 791
+      self.precedence = self.type_of_service >> 5
+      self.total_length = ipheader[2]
+      self.identification = ipheader[3]
+      self.flags = ipheader[4] >> 13
+      self.fragment_offset = (ipheader[4] << 3) >> 3
+      self.time_to_live = ipheader[5]
+      self.protocol = ipheader[6]
+      self.header_checksum = ipheader[7]
+      self.source_address = ipheader[8]      # IPs stored as bytes
+      self.destination_address = ipheader[9]
+    elif self.ihl > 5:
+      # TODO implement IP datagrams headers longer than 20 bytes :-)
+      raise Exception("Not implemented yet")
+
+  def __str__(self):
+    s = ""
+    s += "Type of service: %d\n" % self.type_of_service
+    s += "Total length: %d\n" % self.total_length
+    s += "Identification %s\n" % hex(self.identification) # display as hex
+    s += "ToS Precedence: %s\n" % self.TypeOfServicePrecedence[self.precedence]
+    s += "Time to live: %d\n" % self.time_to_live
+    s += "Protocol: %d\n" % self.protocol
+    s += "Header checksum: %s\n" % hex(self.header_checksum)
+    s += "Source IP: %s\n" % self.inet_ntoa(self.source_address)
+    s += "Destination IP: %s\n" % self.inet_ntoa(self.destination_address)
+    return s
+
 
 # Converts a string of 6 characters of ethernet address into a dash separated hex string
 def eth_addr(a):
@@ -122,6 +141,11 @@ while 1:
 
   frm = Ethernet_frame(packet)
   print(str(frm))
+
+  if frm.e_type == 0x0800:
+    print("ETHERNET DATAGRAM")
+    datagram = IPv4(packet[14:])
+    print(str(datagram))
 
   continue
 
